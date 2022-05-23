418b8a107ca830d581621aa129cf1e3cdc0a9b98
Franco Fernando <69932537+FrancoFernando@users.noreply.github.com>
2022-05-23 10:52:13 +0200

    Update out.cs

--- functions/out.cs
+++ functions/out.cs
@@ -1,1 +1,9 @@
+public static int Div(int x, int y, out int remainder)
+{
+    remainder = x % y;
+    return x / y;
+}
 
+int q = Divide(10, 3, out int r);
+
+int value = int.TryParse(text, out int x) ? x : 0;
